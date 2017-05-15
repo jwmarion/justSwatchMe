67f2dacaff04e8c23ed2fb865c1e6d2e47b6c002
James M <jwmarion@gmail.com>
2017-05-15 10:56:55 -0400

    changed default photo

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ $(function() {
 
   var img = new Image;
   // This src needs the correct headers
-  img.src = "./pic.png";
+  img.src = "./pic.jpg"
   // This makes it work
   img.crossOrigin = "anonymous";
   document.getElementById("my-image").src=img.src;
