338019ea53054c6f4f2a918e52a0eb0354471d00
James M <jwmarion@gmail.com>
2017-05-16 12:08:17 -0400

    testing

--- main.js
+++ main.js
@@ -1,97 +1,4 @@
 $(function() {
-// // //
-// // // var img = document.getElementById('my-image');
-// // console.log('test');
-// // // var canvas = document.getElementById('myCanvas');
-// // // canvas.width = img.width;
-// // // canvas.height = img.height;
-// // // canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
-// // // });
-// // var myCanvas = document.getElementById('myCanvas');
-// // var ctx = myCanvas.getContext('2d');
-// // var img = new Image;
-// // img.onload = function(){
-// //   ctx.drawImage(img,0,0); // Or at whatever offset you like
-// // };
-// // img.src = "./pic.jpg";
-// //
-// // $('canvas').mousemove(function(e) {
-// //
-// //     // if(!this.canvas) {
-// //     //     this.canvas = $('<canvas />')[0];
-// //     //     this.canvas.width = this.width;
-// //     //     this.canvas.height = this.height;
-// //     //     this.canvas.getContext('2d').drawImage(this, 0, 0, this.width, this.height);
-// //     // }
-// // console.log(myCanvas);
-// //     var pixelData = myCanvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data;
-// //
-// //     $('#output').html('R: ' + pixelData[0] + '<br>G: ' + pixelData[1] + '<br>B: ' + pixelData[2] + '<br>A: ' + pixelData[3]);
-// //
-// //
-// // });
-// // });
-// $(function() {
-//
-//
-//   var img = new Image();
-//   // This src needs the correct headers
-//   img.src = "./pic.jpg";
-//   // This makes it work
-//   img.crossOrigin = "anonymous";
-//   document.getElementById("my-image").src=img.src;
-//
-//   var color = [0,0,0,0];
-//   var num = 0;
-//   var pixelData;
-//   if(!this.canvas) {
-//       this.canvas = $('<canvas />')[0];
-//     }
-//
-//   console.log(this.canvas.getContext('2d').getImageData(0, 0, 1, 1).data);
-//
-//   for(var w = 0; w<this.canvas.width; w++){
-//     for(var h = 0; h<this.canvas.height; h++){
-//       pixelData = this.canvas.getContext('2d').getImageData(w, h, 1, 1).data;
-//       color[0] += pixelData[0];
-//       color[1] += pixelData[1];
-//       color[2] += pixelData[2];
-//       color[3] += pixelData[3];
-//       num +=1;
-//     }
-//   }
-// console.log(color);
-//  $('#output').html('R: ' + color[0] + '<br>G: ' + color[1] + '<br>B: ' + color[2] + '<br>A: ' + pixelData[3]);
-//  var c = 'rgba('+pixelData[0]/num+','+pixelData[1]/num+','+pixelData[2]/num+','+pixelData[3]/num+')';
-//  $('#output').css('backgroundColor',c);
-//
-// // $('img').mousemove(function(e) {
-//     //
-//     //     if(!this.canvas) {
-//     //         this.canvas = $('<canvas />')[0];
-//     //         this.canvas.width = this.width;
-//     //         this.canvas.height = this.height;
-//     //         this.canvas.getContext('2d').drawImage(this, 0, 0, this.width, this.height);
-//     //     }
-//     //
-//     //     var pixelData = this.canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data;
-//     //
-//     //     $('#output').html('R: ' + pixelData[0] + '<br>G: ' + pixelData[1] + '<br>B: ' + pixelData[2] + '<br>A: ' + pixelData[3]);
-//     //
-//     //     color = 'rgba('+pixelData[0]+','+pixelData[1]+','+pixelData[2]+','+pixelData[3]+')';
-//     //     $('#output').css('backgroundColor',color);
-//     // });
-//
-// // console.log(img.width  +"   "+ img.height);
-//
-//
-//
-//
-// });
-// });
-
-
-
 var img = new Image();
 img.src = './pic.jpg';
 var canvas = document.getElementById('myCanvas');
@@ -100,7 +7,32 @@ img.onload = function() {
   ctx.drawImage(img, 0, 0);
   img.style.display = 'none';
 };
+
 var color = document.getElementById('colorDisplay');
+var color2 = document.getElementById('colorDisplay2');
+var avg = [0,0,0,0];
+var pCount = 0;
+var data;
+function averageColor(){
+  for(var x = 0; x < ctx.width; x++){
+    for(var y = 0; y<ctx.height; y++){
+      var pixel = ctx.getImageData(x, y, 1, 1);
+       data = pixel.data;
+      avg[0]=avg[0]+data[0];
+      avg[1]=avg[1]+data[1];
+      avg[2]=avg[2]+data[2];
+      avg[3]=avg[3]+data[3];
+      pCount +=1;
+    }
+  }
+  avg = [data[0]/pCount,data[1]/pCount,data[2]/pCount,data[3]/pCount];
+  color.style.background = 'rgba(' + avg[0] + ', ' + avg[1] +
+             ', ' + avg[2] + ', ' + (avg[3] / 255) + ')';
+}
+
+
+
+
 function pick(event) {
   var x = event.layerX;
   var y = event.layerY;
